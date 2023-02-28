06220ea4b87485a76e2c447e3b473f23917a2c41
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2023-02-27 23:01:14 -0300

    history

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+    let mut my_arr: Vec<String> = Vec::new();
+
+    my_arr.push("¡Bienvenido".to_string());
+    my_arr.push("a".to_string());
+    my_arr.push("Platzi!".to_string());
+
+    println!("{:?}", my_arr);
 }
